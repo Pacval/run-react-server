3a6a747fb39b71b2c79875a27c78a19c6ffc38c4
Valentin ROCHET <val.rochet@gmail.com>
2019-12-27 18:05:01 +0100

    Add authentication

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const low = require("lowdb");
 const FileSync = require("lowdb/adapters/FileSync");
 
 const storyLevelAdapter = new FileSync("./tables/story-levels.json");
-const storyLevel = low(storyLevelAdapter);
+const storyLevelTable = low(storyLevelAdapter);
 
 const communityLevelAdapter = new FileSync("./tables/community-levels.json");
-const communityLevel = low(communityLevelAdapter);
+const communityLevelTable = low(communityLevelAdapter);
+
+const userAdapter = new FileSync("./tables/users.json");
+const userTable = low(userAdapter);
 
 const express = require("express");
 const cors = require("cors");
@@ -20,23 +23,48 @@ const port = 8000;
 const host = process.env.HOST;
 
 server.get("/story-level", (req, res) => {
-  const data = storyLevel.get("levels").value();
+  const data = storyLevelTable.get("levels").value();
   res.json({ payload: data });
 });
 
 server.get("/community-level", (req, res) => {
-  const data = communityLevel.get("levels").value();
+  const data = communityLevelTable.get("levels").value();
   res.json({ payload: data });
 });
 
 server.post("/community-level", jsonParser, (req, res) => {
   const level = req.body;
-  const data = communityLevel.get("levels").value() || [];
+  const data = communityLevelTable.get("levels").value() || [];
 
   const newData = data.concat(level);
-  const d = communityLevel.set("levels", newData).write();
+  const d = communityLevelTable.set("levels", newData).write();
 
   res.json(d);
+});
+
+server.get("/user", (req, res) => {
+  const data = userTable.get("users").value();
+  res.json({ payload: data });
+});
+
+server.post("/user", jsonParser, (req, res) => {
+  const user = req.body;
+  const data = userTable.get("users").value() || [];
+
+  const newData = data.concat(user);
+  const d = userTable.set("users", newData).write();
+
+  res.json(d);
+});
+
+server.post("/authenticate", jsonParser, (req, res) => {
+  const user = req.body;
+  const data = userTable.get("users").value() || [];
+
+  const d = data.find(
+    u => u.username === user.username && u.password === user.password
+  );
+  res.json({ payload: !!d ? d.username : null });
 });
 
 /* 404 */
